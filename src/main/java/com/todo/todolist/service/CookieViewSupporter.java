b4cf0c87c929788a03f45f6685f048aa15274da1
WoojinDeve <jwj060111@gmail.com>
2023-04-15 04:44:38 +0900

    refactor: stream map 적용

--- src/main/java/com/todo/todolist/service/CookieViewSupporter.java
+++ src/main/java/com/todo/todolist/service/CookieViewSupporter.java
@@ -1,17 +1,15 @@
 package com.todo.todolist.service;
 
-import static java.lang.Long.parseLong;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
 import static org.springframework.util.StringUtils.hasText;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 @Component
 public class CookieViewSupporter {
@@ -19,9 +17,6 @@ public class CookieViewSupporter {
     //COOKIE SET : 2020-01-11/1&2020-01-11/2 ...
     private static final String DATE_DELIMITER = "&";
     private static final String KEY_VALUE_DELIMITER = "/";
-
-    private static final int KEY_INDEX = 0;
-    private static final int VALUE_INDEX = 1;
 
 
     public boolean isFirstView(final String log, final Long todoListId) {
@@ -32,18 +27,9 @@ public class CookieViewSupporter {
     }
 
     private Map<String, List<Long>> extract(final String logs){
-        Map<String, List<Long>> logDB = new HashMap<>();
-        Arrays.stream(logs.split(DATE_DELIMITER)).forEach(dateLog -> initializeLogs(logDB, dateLog));
-        return logDB;
-    }
-
-    private void initializeLogs(final Map<String, List<Long>> logs,
-                                final String dateLog){
-        final String[] infos = dateLog.split(KEY_VALUE_DELIMITER);
-        if (infos.length == 2 && hasText(infos[KEY_INDEX]) && hasText(infos[VALUE_INDEX])) {
-            logs.put(infos[KEY_INDEX], logs.getOrDefault(infos[KEY_INDEX], new ArrayList<>()));
-            logs.get(infos[KEY_INDEX]).add(parseLong(infos[VALUE_INDEX]));
-        }
+        return Arrays.stream(logs.split(DATE_DELIMITER))
+                .map(log -> log.split(KEY_VALUE_DELIMITER))
+                .collect(groupingBy(log -> log[0], mapping(log -> Long.parseLong(log[1]), toList())));
     }
 
     public String updateLog(final String log, final Long todoListId){
